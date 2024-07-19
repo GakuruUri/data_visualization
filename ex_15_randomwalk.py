85f86bd284566cbf20f5700e69c4e6948c8885d4
GakuruUri <urigakuru@icloud.com>
2024-07-19 21:41:13 +0300

    Aki usingizigit add .!

---
+++ ex_15_randomwalk.py
@@ -0,0 +1,9 @@
+class RandomWalk:
+    def __init__(self, num_points=5000):
+        self.num_points = num_points
+
+        self.x_values - [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        While len(self.x_values) < self.num_points
--- molecular_motion.py
+++ molecular_motion.py
@@ -1,24 +1,26 @@
 import matplotlib.pyplot as plt
 from random_walks import RandomWalk
 
-# Keep making new walks, as long as the program is active
+# Keep making new walks as long as the program is active
+
 while True:
-    #  Make a random walk
-    rw = RandomWalk(50_000)
+
+    # Make a random walk.
+    rw = RandomWalk()
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
     fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
+    # fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
-    ax.set_aspect('equal')
+    ax.plot(rw.x_values, rw.y_values, linewidth=3)
 
-    # Emphasize the first and last points
+    # Emphasize the first and the last points
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
-    # Remove the axes
+    # Removed axes
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
