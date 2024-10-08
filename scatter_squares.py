8c3140fd3ad87ff71703aef75ab8f08b54cd3201
GakuruUri <urigakuru@icloud.com>
2024-10-08 20:40:36 +0300

    Defining Custom Colors

--- scatter_squares.py
+++ scatter_squares.py
@@ -12,7 +12,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use("Solarize_Light2")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=10)
 
 
 #Plotting and Styling Individual Points with scatter()
@@ -27,6 +27,7 @@ ax.tick_params(labelsize=14)
 
 # Set the range for each axis
 ax.axis([0, 1100, 0, 1_100_000])
+# ax.ticklabel_format(style='plain')
 
 
 plt.show()
