e54e04f6811308f87aec288b404bb50389db637b
GakuruUri <urigakuru@icloud.com>
2024-10-08 20:43:06 +0300

    Using a Colormap

--- scatter_squares.py
+++ scatter_squares.py
@@ -12,7 +12,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use("Solarize_Light2")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 
 #Plotting and Styling Individual Points with scatter()
