63e7139803c83d02e6df797bab0aa2bee2f7e02d
GakuruUri <urigakuru@icloud.com>
2024-10-09 20:36:24 +0300

    Plotting the Random Walk

--- random_walks/random_walk.py
+++ random_walks/random_walk.py
@@ -1,20 +1,28 @@
+"""
+create a RandomWalk class
+The class needs
+three attributes: one variable to track the number of points in the walk, and
+two lists to store the x- and y-coordinates of each point in the walk
+"""
+
 from random import choice
 
-
 class RandomWalk:
-    """A class to generate ran """
+    """ A class to generate random walks."""
 
     def __init__(self, num_points=5000):
-        """Initialize attributes of a walk."""
+        """ Initialize attributes of a walk."""
         self.num_points = num_points
 
-        # All walks starts at (0, 0)
+        # All walks start at (0, 0)
         self.x_values = [0]
         self.y_values = [0]
 
+
     def fill_walk(self):
-        """Calculate all the points in the walk."""
-        # Keep taking steps until the walk reached the desired length.
+        """ Calculate all points in the walk."""
+
+        # Keep taking steps until the walk reaches the desired length
         while len(self.x_values) < self.num_points:
 
             # Decide which direction to go, and how far to go
@@ -26,7 +34,8 @@ class RandomWalk:
             y_distance = choice([0, 1, 2, 3, 4])
             y_step = y_direction * y_distance
 
-            # Reject steps that go nowhere.
+
+            # Reject moves that go nowhere
             if x_step == 0 and y_step == 0:
                 continue
 
@@ -36,31 +45,3 @@ class RandomWalk:
 
             self.x_values.append(x)
             self.y_values.append(y)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
